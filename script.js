43b622871533a8a39542e574413b728297ca3223
Jonas Gomes <rawkusperhaps@gmail.com>
2023-06-25 09:24:52 +0100

    Popup window in art page

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let darkModeSave = localStorage.getItem("darkMode");
 let moonIcon = document.querySelector(".ball");
+let grid = document.querySelectorAll(
+  ".gallery-container .grid-photos .column .grid-img .element "
+);
+let gridArray = [...grid];
+let indexNumber = 0;
 const isDarkMode =
   window.matchMedia && window.matchMedia("(prefers-color-scheme:dark").matches;
 logoColor();
@@ -58,3 +63,47 @@ function logoColor() {
     }
   }
 }
+function popupGrid(content, number) {
+  let myH2 = document.getElementsByTagName("h2")[number].innerHTML;
+  let myP = document.getElementsByTagName("p")[number].innerHTML;
+  let myHref = document.getElementsByClassName("hreff")[number].href;
+  content = gridArray[number];
+  document.querySelector(".pop-up").style.display = "block";
+  if (content.tagName == "IMG") {
+    document.querySelector(".pop-up img").style.display = "block";
+    document.querySelector(".pop-up img").src = content.getAttribute("src");
+    document.querySelector(".pop-up video").style.display = "none";
+  } else {
+    document.querySelector(".pop-up video").style.display = "block";
+    document.querySelector(".pop-up video").src = content.getAttribute("src");
+    document.querySelector(".pop-up img").style.display = "none";
+  }
+  document.querySelector(".pop-up h2").innerHTML = myH2;
+  document.querySelector(".pop-up p").innerHTML = myP;
+  document.querySelector(".pop-up a").href = myHref;
+  document.querySelector(".pop-up i.bxs-right-arrow").onclick = () => {
+    number++;
+    if (number > 21) {
+      number = 0;
+    }
+    popupGrid(content, number);
+  };
+  document.querySelector(".pop-up i.bxs-left-arrow").onclick = () => {
+    number--;
+    if (number < 0) {
+      number = 21;
+    }
+    popupGrid(content, number);
+  };
+}
+
+gridArray.forEach((image) => {
+  image.onclick = () => {
+    indexNumber = gridArray.indexOf(image);
+    popupGrid(image, indexNumber);
+  };
+});
+
+document.querySelector(".pop-up span").onclick = () => {
+  document.querySelector(".pop-up").style.display = "none";
+};
